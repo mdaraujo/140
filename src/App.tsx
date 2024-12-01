6ce5874d5e0a4ce7abe6003c9b5f7fe7e0fee48c
Miguel Araújo <maraujo@broadvoice.com>
2024-12-01 00:12:13 +0000

    feat: e agora? with animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      {/* TODO footer */}
+      {/* <h1>Associação 140</h1> */}
+
+      <div className="question">
+        <a href="https://forms.gle/dgnQgUeGjRHgjG2E7" target="_blank">
+          <p className="rotate1 l1">E</p>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://forms.gle/dgnQgUeGjRHgjG2E7" target="_blank">
+          <p className="l2">AGORA</p>
+        </a>
+        <a href="https://forms.gle/dgnQgUeGjRHgjG2E7" target="_blank">
+          <p className="rotate2 l3">?</p>
         </a>
       </div>
-      <h1>Associação 140</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
