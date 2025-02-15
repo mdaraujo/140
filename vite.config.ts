d7f28cdd58061544c314efce3cb42454d3792757
Miguel Araújo <maraujo@broadvoice.com>
2025-02-15 12:01:05 +0000

    chore: add config to deploy with gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Only needed when deploying in gh-pages
+  base: '/140',
 });
+
+// To deploy to gh-pages:
+// npm run deploy
