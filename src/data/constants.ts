7cfaa6a0ac4764b2947e16f7072f93d2b967dc5c
Miguel Araújo <maraujo@broadvoice.com>
2025-08-10 14:09:16 +0100

    feat: added new gumajazz position

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,7 +1,7 @@
 export const LOCATIONS = {
   cafeSociedade: 'https://maps.app.goo.gl/6b4hyN2v7zgYSLDb7',
   fidelis: 'https://maps.app.goo.gl/vGUHBfTgUpr96E7z7',
-  gumaJazz: 'https://maps.app.goo.gl/2SQxqcDrJwXRiRz98',
+  gumaJazz: 'https://maps.app.goo.gl/KbuSZ5HesDFNqh5J9',
 };
 
 export const FORMS = {
